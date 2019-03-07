63e80411ae1cb7e7b24ef2d31192591625562203
남궁성환 <saturn203@lotte.net>
2019-03-07 11:32:13 +0900

    dd

--- src/test/java/com/app/EduApiApplicationTests.java
+++ src/test/java/com/app/EduApiApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.app.edu.EduApiApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = EduApiApplication.class)
 public class EduApiApplicationTests {
 
 	@Test
